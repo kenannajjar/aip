b504224bedcf0a37797cace9ab24aebca97eb6f0
Kenan Najjar <kenannajjar@Kenans-MBP.lan>
2024-03-02 15:51:23 -0800

    bruh

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import Sidebar from './components/Sidebar/Sidebar';
 import MainContent from './components/MainContent/MainContent';
 import InputArea from './components/InputArea/InputArea';
-import './App.css'; 
+import './App.css';
 
 const App = () => {
   return (
     <div className="app">
       <MainContent />
     </div>
-    
   );
 };
 
