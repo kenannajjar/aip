1677abf31ab0966721636e419a16e2894dcf3957
Kenan Najjar <kenannajjar@Kenans-MBP.lan>
2024-03-03 00:23:01 -0800

    Cleaned up the code

--- src/components/InputArea/InputArea.js
+++ src/components/InputArea/InputArea.js
@@ -1,41 +1,38 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './InputArea.css';
-import { ReactComponent as InputButtonIcon } from '../../assets/inputbutton.svg'; // Assuming SVG is in the same directory
+import { ReactComponent as InputButtonIcon } from '../../assets/inputbutton.svg'; 
 
 const InputArea = ({ onSendMessage }) => {
 
-    // State and refs
     const [inputValue, setInputValue] = useState('');
     const textareaRef = useRef(null);
 
-    // Event handlers
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
     };
 
     const handleSendMessage = (event) => {
         event.preventDefault();
-            onSendMessage(inputValue);
-            console.log(inputValue)
+        if (inputValue.trim() !== '') {
+            onSendMessage(inputValue.trim()); 
+            console.log(inputValue.trim()); 
             setInputValue('');
-    
+        }
     };
+    
 
-    // Handling Enter key press without adding a new line
     const handleKeyDown = (event) => {
         if (event.key === 'Enter' && !event.shiftKey) {
             handleSendMessage(event);
         }
     };
 
-    // Effects
     useEffect(() => {
         textareaRef.current.style.height = "0px";
         const scrollHeight = textareaRef.current.scrollHeight;
         textareaRef.current.style.height = scrollHeight + "px";
     }, [inputValue]);
 
-    // Render method
     return (
         <div className="input-area">
             <div className="text-input-and-button">
